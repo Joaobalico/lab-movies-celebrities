329e5a6823a8836a821e081c84171034b495ba8d
Joao Balico <joaobalico12@hotmail.com>
2022-02-15 22:22:52 +0000

    Create movie model

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -8,7 +8,7 @@ router.get('/celebrities/create', (req, res, next) => {
     Celebrity.find()
       .then((celebrities) => {
         console.log(celebrities);
-        res.render('celebrities/new-celebrity');
+        res.render('celebrities/new-celebrity.hbs');
       })
       .catch((err) => {
         next(err);
@@ -21,13 +21,25 @@ router.post('/celebrities/create', (req, res, next) => {
   
     Celebrity.create({ name, occupation, catchPhrase })
       .then((celebrity) => {
-        console.log('Cel created', celebrity.name);
+        console.log('Celebrity created', celebrity.name);
         res.redirect('/celebrities');
       })
       .catch((err) => {
         next(err)
         res.redirect('/celebrities/create');
       });
-  });
+});
+
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+      .then((celebrities) => {
+        res.render('celebrities/celebrities.hbs', {celebrities: celebrities});
+      })
+      .catch((err) => {
+        next(err);
+      });
+});
+
+
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,5 +2,17 @@
 const router = require("express").Router();
 
 // all your routes here
+const Movie = require('../models/Movie.model')
+
+router.get('/movies/create', (req, res, next) => {
+    Movie.find()
+      .then((movies) => {
+        console.log(movies);
+        res.render('movies/new-movie.hbs');
+      })
+      .catch((err) => {
+        next(err);
+      });
+});
 
 module.exports = router;
